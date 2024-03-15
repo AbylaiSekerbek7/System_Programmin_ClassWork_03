d1973921c6e6eff4f4dc8109f97d7744a7b8a347
Секербек Абылайхан Кайратулы <SekerbekA@lcoast.itstep>
2024-03-15 20:41:05 +0600

    Mutex

--- ClassWork_03/Program.cs
+++ ClassWork_03/Program.cs
@@ -9,6 +9,8 @@ namespace ClassWork_03
 {
     internal class Program
     {
+        static Mutex mutex = new Mutex(false);
+
         static void Main(string[] args)
         {
             for (int i = 0; i < 4; i++)
@@ -24,15 +26,23 @@ namespace ClassWork_03
         static void ThRoutine(object param)
         {
             int id = Thread.CurrentThread.ManagedThreadId;
+            mutex.WaitOne();
+
             Console.WriteLine($"{id}: Thread is started");
+
+            // Попытка захвата мьютекса
+            
             // Hello My Name is Abylaikhan !
             Console.Write("Hello! ");       Thread.Sleep(0);
             Console.Write("My ");           Thread.Sleep(0);
             Console.Write("Name ");         Thread.Sleep(0);
             Console.Write("is ");           Thread.Sleep(0);
             Console.Write("Abylaikhan ");   Thread.Sleep(0);
-            Console.Write("! ");
-            Console.Write($"{id}: Thread is exit");
+            Console.WriteLine("! ");        Thread.Sleep(0);
+            Console.WriteLine($"{id}: Thread is exit");
+
+            // Освободить мьютекс
+            mutex.ReleaseMutex();
         }
     }
 }
